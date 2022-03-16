840978faefed0a5741ba3e7d5bc437b0019d6203
lutfialdrii <12050110415@students.uin-suska.ac.id>
2022-03-16 07:08:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/days2_2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.days2_2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
